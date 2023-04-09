349e3c807ca7cb4ba2a6009fc93ac3c33a9b47bb
qhle2001 <hunglequang1230@gmail.com>
2023-04-10 00:43:33 +0700

    change

--- PROJECT 2_Unilever/database.sql
+++ PROJECT 2_Unilever/database.sql
@@ -1,10 +1,55 @@
 #create database unilever;
 use unilever;
-create table user(
-	id_user integer primary key,
-    user_name char
-);
-create table account(
+create table TaiKhoan(
 	username char primary key,
-    passwords char
+    passwords char,
+    constraint lk foreign key (username) references QuanLiNguoiDung(email)
+);
+create table QuanLiNguoiDung(
+	email char primary key,
+    hoten char,
+    chucvu char,
+    khuvuc char,
+    nguoibaocao char, 
+    sodienthoai char,
+    quanly char
+);
+create table QuyenTruyCap();
+create table QuanLiChucVu(
+
+);
+create table QuanLiLichViengTham(
+	id_lichviengtham char primary key,
+	ngaybatdau date,
+    ngayketthuc date,
+	mucdich char,
+    nguoitao char,
+    trangthai char,
+    nhaphanphoi char
+);
+create table CongViec(
+	id_congviec char primary key,
+    tieude char,
+    nguoithuchien char,
+    nguoibaocao char,
+    mota char,
+    ngaytao date,
+    ngaybatdau date,
+    ngayketthuc date
+);
+create table BinhLuan(
+	id_binhluan char primary key,
+    id_congviec char,
+    nguoibinhluan char,
+    noidungbinhluan char,
+    constraint lk1 foreign key (id_congviec) references CongViec(id_congviec)
+);
+create table DanhSachKhach(
+	emailkhach char,
+    id_congviec char,
+    hotenkhach char,
+    tinhtrang char,
+    primary key (emailkhach, id_congviec),
+    constraint lk2 foreign key (emailkhach) references QuanLiNguoiDung(email),
+    constraint lk3 foreign key (id_congviec) references CongViec(id_congviec)
 );
